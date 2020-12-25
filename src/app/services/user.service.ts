26e03d780b0a3c9077b2815357efd2eb4f699336
Asiri Senith <asirisenith@gmail.com>
2020-12-25 22:51:00 +0530

    created  user,user-add components and user.service file

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,9 @@
+
+export class UserService{
+
+    users = [
+        {name:'Asiri', status:'Active'},
+        {name:'Senith', status:'Active'},
+        {name:'Manjitha', status:'Active'}     
+    ]
+}
